c20bde0eddbf2308dc98ebecee122662328c4e3b
Grant Bell <grantbell@Grants-Air.attlocal.net>
2022-09-17 10:20:12 -0500

    added navbar functionality

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import "./Homepage.css"
 
 function HomePage() {
   return (
     <> 
-    <div>{Navbar}</div>
-    <div>HomePage</div>
+    <h1 className='hp_header'>Welcome to everything you needed to know about Antsy McClain and his musical cohorts, The Trailer Park Troubadours</h1>
     </>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,10 +7,60 @@ function Navbar() {
     return (
         <> 
         <img className="navbar_Logo" src={UnhitchedBanner}/>
-        <ul>
+        <ul className="navbar">
             <div className="navbar__item active">
-                <Link to="">
-                    LINK
+                <Link className='navbar_link' to="">
+                    home
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/tour">
+                    tour
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/store">
+                    store
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/art">
+                    art
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/design">
+                    design
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/bio">
+                    bio
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/video">
+                    video
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/fieldtrips">
+                    field trips
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/woodflock">
+                    wood flock
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/podcast">
+                    podcast
+                </Link>
+            </div>
+            <div className="navbar__item active">
+                <Link className='navbar_link' to="/contact">
+                    contact
                 </Link>
             </div>
         </ul>
