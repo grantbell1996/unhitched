416175985b8854d04f272bd01611c97e7deb6df6
Grant Bell <grantbell@Grants-Air.attlocal.net>
2022-12-03 15:12:38 -0600

    added podcast text and banner

--- src/components/Podcast/Podcast.js
+++ src/components/Podcast/Podcast.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./Podcast.css"
+import PodcastHeader from "../images/Podcast_Header.png"
+import PodcastLogo from "../images/Resisting_Enlightenment.png"
 
 function Podcast() {
-  return (
-    <div>Podcast</div>
-  )
+    return (
+        <div>
+            <img className='podcastHeader' src={PodcastHeader}></img>
+            <div className='podcastText'>Antsy McClain’s new podcast unwisely revisits our most awkward, embarrassing moments, offering only the impotent act of commiseration.
+                “Resisting Enlightenment” presents conversations, stories and music with the philosophy that if we can laugh, we can heal.
+                And along the way, you’ll hear about living a creative life from the most creative and interesting people in Antsy’s wide circle of friends. </div>
+            <img className='podcast_logo' src={PodcastLogo}></img>    
+        </div>
+    )
 }
 
 export default Podcast
